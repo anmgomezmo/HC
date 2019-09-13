0f135863d3684fc4a65394189ba5a624ea19c487
Andres Mauricio Gomez Mongui <anmgomezmo@darkstar.org>
2019-09-13 12:35:57 -0500

    Tarea

---
+++ Tarea1/sumarm.cpp
@@ -0,0 +1,15 @@
+#include <cstdio>
+#include <fstream>
+
+
+int main(void){
+  std::ofstream fout("datos.txt");
+  fout.precision(16);
+  double sum = 0.0; 
+  for(int N = 1; N <= 50000; ++N){
+    sum = sum + 1.0/N;
+    fout << N << "\t" << sum << "\n";
+  }
+  fout.close();
+  return 0;
+}
