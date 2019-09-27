9188d16d8d2cf4c04656147a0416eb6aa87f81e4
Andres Mauricio Gomez Mongui <anmgomezmo@darkstar.org>
2019-09-27 12:48:06 -0500

    Librerias

---
+++ 2019-09-27 standartlibmath/bessel.cpp
@@ -0,0 +1,17 @@
+#include <cstdio>
+#include <cmath>
+
+int main(void){
+  
+  const double xmin = 0.0;
+  const double xmax = 10.0;
+  const double dx = 0.1;
+  const int NSTEPS = (xmax-xmin)/dx;
+
+  for(int ii = 0; ii <= NSTEPS; ++ii){
+    double x = xmin+ii*dx;
+    std::printf("%25.16e \t %25.16e \n", x, std::cyl_bessel_i(1, x));
+  }
+  
+  return 0;
+}
